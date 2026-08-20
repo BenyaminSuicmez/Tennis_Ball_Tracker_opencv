9d18ac3658f812b32ff2e4f15ed2b22d79d42f9a
Benyamin Suicmez <b.suicmez@gmx.de>
2026-08-20 18:32:02 +0200

    Add live ball tracking with color mask and bounding box

--- main.py
+++ main.py
@@ -23,7 +23,21 @@ while True:
 
     mask = cv2.inRange(hsv, lowerb=lower, upperb=upper)
 
-    cv2.imshow("Video", mask)
+
+
+    contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    if contours:
+
+        biggest = max(contours, key=cv2.contourArea)
+
+        x, y, w, h = cv2.boundingRect(biggest)
+
+        cv2.rectangle(frame, pt1=(x, y), pt2=(x+w, y+h), color=(0, 255, 0), thickness=4)
+
+        cv2.putText(frame, "Tennis Ball", org=(x, y-10), fontFace=cv2.FONT_HERSHEY_COMPLEX, color=(0,255, 0), fontScale=2)
+
+    cv2.imshow("Tracker", frame)
 
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
